1cac9883b4f8a70570a8f11f4bc11796342bf5a3
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-03-18 18:39:09 +0600

    order service

---
+++ src/app/modules/orders/order.service.ts
@@ -0,0 +1,62 @@
+import AppError from "../../error/AppError";
+
+import httpStatus from "http-status";
+import { TOrder } from "./order.interface";
+import { Order } from "./orders.model";
+
+const createOrderIntoDB = async (payload: TOrder) => {
+  const result = await Order.create(payload);
+  return result;
+};
+const getAllOrderFromDB = async () => {
+  const result = await Order.find();
+  return result;
+};
+
+const getSingleOrderFromDB = async (id: string) => {
+  // const isUserExists = await Order.findById(id)
+
+  const result = await Order.findById(id);
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, "Order not found");
+  }
+  return result;
+};
+const updateOrderIntoDB = async (id: string, payload: TOrder) => {
+  const isUserExists = await Order.findById(id);
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Order not found");
+  }
+  const result = await Order.findByIdAndUpdate(
+    id,
+    {
+      ...payload,
+    },
+    { new: true, runValidators: true }
+  );
+
+  return result;
+};
+const deleteOrderIntoDB = async (id: string) => {
+  const isUserExists = await Order.findById(id);
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Order not found");
+  }
+  const result = await Order.findByIdAndUpdate(
+    id,
+    {
+      isDeleted: true,
+    },
+    { new: true, runValidators: true }
+  );
+
+  return result;
+};
+
+export const OrderService = {
+  createOrderIntoDB,
+  getAllOrderFromDB,
+  getSingleOrderFromDB,
+  updateOrderIntoDB,
+  deleteOrderIntoDB,
+};
