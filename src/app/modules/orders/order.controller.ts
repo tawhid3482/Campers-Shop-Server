77833d87fa7001fe2966511b39f776f9ffdbd5bf
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-03-19 11:21:28 +0600

    order controller route

---
+++ src/app/modules/orders/order.controller.ts
@@ -0,0 +1,67 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { OrderService } from "./order.service";
+
+const createOrder = catchAsync(async (req, res) => {
+  const result = await OrderService.createOrderIntoDB(req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Order created successfully",
+    data: result,
+  });
+});
+const getAllOrder = catchAsync(async (req, res) => {
+  const result = await OrderService.getAllOrderFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All Order retrieved successfully",
+    data: result,
+  });
+});
+const getSingleOrder = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OrderService.getSingleOrderFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: " Order retrieved successfully",
+    data: result,
+  });
+});
+const deleteOrder = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OrderService.deleteOrderIntoDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: " Order deleted successfully",
+    data: result,
+  });
+});
+const updateOrder = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await OrderService.updateOrderIntoDB(id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: " Order deleted successfully",
+    data: result,
+  });
+});
+
+
+export const OrderControllers ={
+    createOrder,
+    getAllOrder,
+    getSingleOrder,
+    deleteOrder,
+    updateOrder
+}
---
+++ src/app/modules/orders/order.route.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import auth from "../../middlewares/auth";
+import validationRequest from "../../middlewares/ValidationRequest";
+import { orderValidation } from "./orders.validation";
+import { OrderControllers } from "./order.controller";
+
+
+const router = express.Router();
+
+router.post(
+  "/orders",
+  auth("ADMIN", "SUPER_ADMIN"),
+  validationRequest(orderValidation.orderValidationSchema),
+  OrderControllers.createOrder
+);
+router.get("/orders", OrderControllers.getAllOrder);
+router.get(
+  "/orders",
+  auth("ADMIN", "SUPER_ADMIN", "CUSTOMER"),
+  OrderControllers.getSingleOrder
+);
+router.put(
+  "/orders",
+  auth("ADMIN", "SUPER_ADMIN"),
+  OrderControllers.updateOrder
+);
+router.delete(
+  "/orders",
+  auth("ADMIN", "SUPER_ADMIN"),
+  OrderControllers.deleteOrder
+);
